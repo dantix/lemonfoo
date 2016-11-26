5bc7a63faff0ef854147a168e4f03a136a635fe1
Roman Holovin <danteek@gmail.com>
2016-11-27 01:10:51 +0200

    Hello world

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import h from 'lemonjsx';
+import { spawn } from 'child_process';
 
-console.log(<section align="l">Hello world</section>);
+const lemonbar = spawn('/usr/bin/lemonbar', ['-b', '-f "Ubuntu-12"']);
+
+const bar = (
+  <bar>
+    <section align="l">Hello</section>
+    <section align="r">world</section>
+  </bar>
+);
+
+lemonbar.stdin.write(bar);
